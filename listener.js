62e8cf18d4e3cb2df7525b1f6b5362eb7488d0f4
Michael Minter <mothore@gmail.com>
2012-09-22 14:27:42 -0500

    add fracture shortening service

--- listener.js
+++ listener.js
@@ -1,7 +1,24 @@
 chrome.browserAction.onClicked.addListener(function() {
-  chrome.tabs.getSelected(null, function(tab) { //<-- "tab" has all the information
-    chrome.tabs.create({
-      url: "http://tamponapp.com/?p="+tab.title+" "+tab.url
+  chrome.tabs.getSelected(null, function(tab) {
+    return_url(tab.url, function(e) {
+      chrome.tabs.create({
+        url: "http://tamponapp.com/?p="+tab.title+" "+e
+      });
     });
   });
 });
+
+function return_url(url, callback) {
+  if (url.length > 20) {
+    $.ajax({
+      type: 'POST',
+      url: 'http://fracture.it',
+      data: { url: url },
+      dataType: 'JSON'
+    }).done(function(e) {
+      callback(e.fractured_url);
+    });
+  } else {
+   callback(url);
+  }
+}
